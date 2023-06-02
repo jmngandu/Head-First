bba7e5c889d451579f1361db6aaf4d6fd97fa345
James Ngandu <jamesmngandu@gmail.com>
2023-06-02 18:47:20 +0300

    added

--- python/ch2/dogcalc.py
+++ python/ch2/dogcalc.py
@@ -1,7 +1,7 @@
 dogs_name = input("What is your dogs name?")
-print(dogs_name)
 dogs_age = input('What is your dogs age?')
-
 human_age =int (dogs_age) * 7
+print(dogs_name)
+print(dogs_age)
 
 print(human_age)
