ff1a19414a39622ccb8add2de4e4eecf754e2d75
James Ngandu <jamesmngandu@gmail.com>
2023-06-07 22:23:12 +0300

    benz

---
+++ python/ch3/DreamCar.py
@@ -0,0 +1,16 @@
+# This code supports my yearly habit of seeing if I can
+# afford a mercedes. The general algorithm is to compare
+# my bank account amount to the current cost of a mercedes.
+bank_balance = 40,000000;
+mercedes_cost = 12,000000
+if bank_balance >= mercedes_cost:
+# If it's greater I can finally buy one.
+    print('Why not?')
+    print('Go ahead, buy it')
+else:
+    print('Sorry')
+    print('Try again next year')
+# bummer
+
+
+
