728724068ea4fcf7aa4f187556782b393477ac25
James Ngandu <jamesmngandu@gmail.com>
2023-06-07 21:40:30 +0300

    added

---
+++ python/ch3/rock.py
@@ -0,0 +1,11 @@
+import random
+random_choice = random.randint(0,2)
+if random_choice == 1:
+    computer_choice = 'rock'
+elif random_choice == 2:
+    computer_choice = 'paper'
+elif random_choice == 3:
+    computer_choice = 'scissor'
+    
+
+
