c386638d32942bb611d6d811268d8b33c0946af1
James Ngandu <jamesmngandu@gmail.com>
2023-06-07 22:58:41 +0300

    added

---
+++ python/ch3/guessing_game.py
@@ -0,0 +1,7 @@
+color = 'blue'
+guess = ''
+guesses = 0
+while guess != color:
+    guess = input('What color am I thinking of? ')
+    guesses = guesses + 1
+print('You got it! It took you', guesses, 'guesses')
