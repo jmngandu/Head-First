9b7c3915480a2a37fa0c73acb28472d8abadfcd5
James Ngandu <jamesmngandu@gmail.com>
2023-06-08 22:08:06 +0300

    added

--- python/ch4/range.py
+++ python/ch4/range.py
@@ -1,2 +1,8 @@
-for i in range(5):
+for i in range(2): #range makes the sequence 0, 1
     print('Iterating through', i)
+
+smoothies = ['coconut','strawberry','banana','tropical','acai berry']
+i = 0
+length = len(smoothies)
+for i in range(length):
+    print('smoothes ' +str(i), smoothies[i])
