b8f02ee71c1c7967d70ebe4ffc0d9b9359213b5f
James Ngandu <jamesmngandu@gmail.com>
2023-06-08 04:49:01 +0300

    added

---
+++ python/ch4/items.py
@@ -0,0 +1,6 @@
+scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54]
+score = scores[3]
+print('Solution #3 produced', score, 'bubbles.')
+smoothies = ['coconut', 'strawberry', 'banana', 'pineapple', 'acai berry']
+smooth  = smoothies[2]
+print('i love eating',smooth,'do you like it ?')
