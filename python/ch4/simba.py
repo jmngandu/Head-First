03fcde57f02dc80a46d5e28c6cf1634f1772a63a
James Ngandu <jamesmngandu@gmail.com>
2023-06-09 00:05:59 +0300

    added

--- python/ch4/simba.py
+++ python/ch4/simba.py
@@ -1,21 +1,3 @@
 high_score = 0
 length = len(scores)
 for i in range(length):
-print('Bubble solution #' + str(i), 'score:', scores[i])
-if scores[i] > high_score:
-high_score = scores[i]
-print('Bubbles tests:', length)
-print('Highest bubble score:', high_score)
-best_solutions = []
-for i in range(length):
-if high_score == scores[i]:
-best_solutions.append(i)
-print('Solutions with the highest score:'
-igh_score = scores[i]
-print('Bubbles tests:', length)
-print('Highest bubble score:', high_score)
-best_solutions = []
-for i in range(length):
-if high_score == scores[i]:
-best_solutions.append(i)
-print('Solutions with the highest score:'
