afb13de1b0ed68bccf803b742f0edaeb79ef7786
James Ngandu <jamesmngandu@gmail.com>
2023-06-08 05:03:47 +0300

    added

--- python/ch4/items.py
+++ python/ch4/items.py
@@ -4,3 +4,7 @@ print('Solution #3 produced', score, 'bubbles.')
 smoothies = ['coconut', 'strawberry', 'banana', 'pineapple', 'acai berry']
 smoothies[2] = 'tropical'
 print('i love eating',smoothies,'do you like it ?')
+length = len(smoothies)#tells you how many items are in the list
+print(length)
+last =smoothies[length -1]#getting the last item
+print(last)
