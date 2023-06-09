f44ea741c49276ee4327721f7827d2469bda288e
James Ngandu <jamesmngandu@gmail.com>
2023-06-10 00:36:41 +0300

    swap

--- python/ch4-2/swap.py
+++ python/ch4-2/swap.py
@@ -2,3 +2,8 @@ number_of_scores = len(scores) #Get the length of the list.
 solution_numbers = list(range(number_of_scores)) #Create a range from 0 to the length of the list (minus 1) and then
                                                  #use the list function to convert the understand your direction.
                                                  #range into a list [0, 1, 2, ... ].
+print('Top Bubble Solutions')
+for i in range(0,5):
+    print(str(i+1) + ')',
+        'Bubble solution #' + str(solution_numbers[i]),
+        'score:', scores[i])
