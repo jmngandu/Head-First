1ce190c0ffee7d66c47e94fb3bfb1c7803514b13
James Ngandu <jamesmngandu@gmail.com>
2023-06-09 17:58:02 +0300

    codie

---
+++ python/ch5/dog_codie.py
@@ -0,0 +1,24 @@
+dog_name = "Codie";
+dog_weight = 40
+if dog_weight > 20:
+    print(dog_name, 'says WOOF WOOF')
+else:
+    print(dog_name, 'says woof woof')
+dog_name = "Sparky"
+dog_weight = 9
+if dog_weight > 20:
+    print(dog_name, 'says WOOF WOOF')
+else:
+    print(dog_name, 'says woof woof')
+dog_name = "Jackson"
+dog_weight = 12
+if dog_weight > 20:
+    print(dog_name, 'says WOOF WOOF')
+else:
+    print(dog_name, 'says woof woof')
+dog_name = "Fido"
+dog_weight = 65
+if dog_weight > 20:
+    print(dog_name, 'says WOOF WOOF')
+else:
+    print(dog_name, 'says woof woof')
