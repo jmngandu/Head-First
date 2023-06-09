a727262538eef148d9c40982e01e3aff157810b3
James Ngandu <jamesmngandu@gmail.com>
2023-06-10 00:26:16 +0300

    added

--- python/ch4-2/swap.py
+++ python/ch4-2/swap.py
@@ -1,2 +1,4 @@
-number_of_scores = len(scores)
-solution_numbers = list(range(number_of_scores))
+number_of_scores = len(scores) #Get the length of the list.
+solution_numbers = list(range(number_of_scores)) #Create a range from 0 to the length of the list (minus 1) and then
+                                                 #use the list function to convert the understand your direction.
+                                                 #range into a list [0, 1, 2, ... ].
