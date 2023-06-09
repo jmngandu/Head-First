e9a52c58470e34b7f013a0e6df221c8c82341f96
James Ngandu <jamesmngandu@gmail.com>
2023-06-09 19:09:52 +0300

    avatar

--- python/ch5/avatar.py
+++ python/ch5/avatar.py
@@ -1,24 +1,24 @@
 hair = input("What color hair [brown]? ")
 if hair == '':
-hair = 'brown'
-print('You chose', hair)
+    hair = 'brown'
+    print('You chose', hair)
 hair_length = input("What hair length [short]? ")
 if hair_length == '':
-hair_length = 'short'
-print('You chose', hair_length)
+    hair_length = 'short'
+    print('You chose', hair_length)
 eyes = input("What eye color [blue]? ")
 if eyes == '':
-eyes = 'blue'
-print('You chose', eyes)
+    eyes = 'blue'
+    print('You chose', eyes)
 gender = input("What gender [female]? ")
 if gender == '':
-gender = 'female'
-print('You chose', gender)
+    gender = 'female'
+    print('You chose', gender)
 has_glasses = input("Has glasses [no]? ")
 if has_glasses == '':
-has_glasses = 'no'
-print('You chose', has_glasses)
+    has_glasses = 'no'
+    print('You chose', has_glasses)
 has_beard = input("Has beard [no]? ")
 if has_beard == '':
-has_beard = 'no'
-print('You chose', has_beard)
+    has_beard = 'no'
+    print('You chose', has_beard)
