6084acb6c3bedd882585cf40071ba4e13222a600
James Ngandu <jamesmngandu@gmail.com>
2023-06-10 00:37:47 +0300

    swap

--- python/ch4-2/swap.py
+++ python/ch4-2/swap.py
@@ -1,3 +1,6 @@
+scores = [60, 50, 60, 58, 54, 54, 58, 50, 52, 54, 48, 69,
+34, 55, 51, 52, 44, 51, 69, 64, 66, 55, 52, 61,
+46, 31, 57, 52, 44, 18, 41, 53, 55, 61, 51, 44]
 number_of_scores = len(scores) #Get the length of the list.
 solution_numbers = list(range(number_of_scores)) #Create a range from 0 to the length of the list (minus 1) and then
                                                  #use the list function to convert the understand your direction.
