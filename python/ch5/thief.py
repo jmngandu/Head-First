64ac5e68d5e77c58cb62f885d574356819b5ae5c
James Ngandu <jamesmngandu@gmail.com>
2023-06-09 23:19:58 +0300

    thief

---
+++ python/ch5/thief.py
@@ -0,0 +1,12 @@
+balance = 10500
+camera_on = True
+def steal(balance, amount):
+    global camera_on
+    camera_on = False
+    if (amount < balance):
+        balance = balance - amount
+
+    return amount
+    camera_on = True 
+proceeds = steal(balance, 1250)
+print('Criminal: you stole', proceeds)   
