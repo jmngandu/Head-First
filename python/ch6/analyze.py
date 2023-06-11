e376379c8548637637c72eb6692f17aef9b76056
James Ngandu <jamesmngandu@gmail.com>
2023-06-11 20:46:28 +0300

    text

--- python/ch6/analyze.py
+++ python/ch6/analyze.py
@@ -4,3 +4,5 @@ def compute_readability(text):#function that takes text as a parameter
     total_sentences = 0
     total_syllables = 0
     score = 0
+    print(text)
+compute_readability(ch1text.text)
--- python/ch6/ch1text.py
+++ python/ch6/ch1text.py
@@ -1,4 +1,4 @@
 text = """The first thing that stands between you and writing your first, real,
 piece of code, is learning the skill of breaking problems down into
 achievable little actions that a computer can do for you."""
-print(text)
+
