09299404f73144dc22e5b60b0c3d9c02082cd1cc
James Ngandu <jamesmngandu@gmail.com>
2023-06-11 20:35:56 +0300

    text

---
+++ python/ch6/text.py
@@ -0,0 +1,3 @@
+text = """The first thing that stands between you and writing your first, real,
+piece of code, is learning the skill of breaking problems down into
+achievable little actions that a computer can do for you."""
