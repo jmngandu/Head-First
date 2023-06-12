0e07e109a5b01877984a40abcbfb22388037ab13
James Ngandu <jamesmngandu@gmail.com>
2023-06-12 21:20:47 +0300

    char

--- python/ch6/analyze.py
+++ python/ch6/analyze.py
@@ -1,4 +1,12 @@
 import ch1text
+
+def count_sentences(text):
+    count = 0
+    for char in text:
+        if char == '.' or char == ';' or char == '?' or char == '!':
+            count = count + 1
+            
+    return count
 def compute_readability(text):#function that takes text as a parameter
     total_words = 0
     total_sentences = 0
