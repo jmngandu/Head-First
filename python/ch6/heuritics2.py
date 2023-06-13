b9011adf3ceaf52bab33ba9b1e1787d8849d41f9
James Ngandu <jamesmngandu@gmail.com>
2023-06-13 23:16:17 +0300

    heuristics

--- python/ch6/heuritics2.py
+++ python/ch6/heuritics2.py
@@ -1,3 +1,4 @@
+
 def compute_readability(text):
     total_words = 0
     total_sentences = 0
@@ -7,3 +8,9 @@ def compute_readability(text):
     total_words = len(words)
     total_sentences = count_sentences(text)
     total_syllables = count_syllables(words)
+score = (206.835 - 1.015 * (total_words / total_sentences)
+                    - 84.6 * (total_syllables / total_words))
+
+print(total_sentences, 'sentences')
+print(total_syllables, 'syllables')
+print(score, 'reading ease score')
