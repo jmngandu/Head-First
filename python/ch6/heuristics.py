dc6a2d4fb14189a5ac83fa91314a3ac970e686ee
James Ngandu <jamesmngandu@gmail.com>
2023-06-13 16:25:40 +0300

    heuristics

---
+++ python/ch6/heuristics.py
@@ -0,0 +1,5 @@
+Finding syllables is a messy business because the English language is messy. For instance, why does “walked” have
+one syllable while “loaded” is two? English is full of such inconsistencies.
+For problems like this we don’t write algorithms, we write heuristics. A heuristic is a lot like an algorithm, only it’s
+not a 100% solution. It might, for instance, solve a problem with a good answer, but not necessarily a perfect
+answer.
