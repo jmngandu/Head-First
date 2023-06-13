bd49ad2658fa807e06c4ad42693e8b3f62512d8e
James Ngandu <jamesmngandu@gmail.com>
2023-06-13 20:30:34 +0300

    heuristics

--- python/ch6/heuristics.py
+++ python/ch6/heuristics.py
@@ -12,3 +12,7 @@ def count_syllables(words):
 def count_syllables_in_word(word):
     count = 0
     return count
+
+    lyrics = 'I heard you on the wireless back in fifty two'
+    my_substring = lyrics[2:7]
+    print(my_substring)
