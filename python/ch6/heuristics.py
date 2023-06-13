83a88d0a6a38afa2f6c8349aadb97fd3e391dfaa
James Ngandu <jamesmngandu@gmail.com>
2023-06-13 16:50:07 +0300

    h

--- python/ch6/heuristics.py
+++ python/ch6/heuristics.py
@@ -1,5 +1,14 @@
-Finding syllables is a messy business because the English language is messy. For instance, why does “walked” have
-one syllable while “loaded” is two? English is full of such inconsistencies.
-For problems like this we don’t write algorithms, we write heuristics. A heuristic is a lot like an algorithm, only it’s
+#Finding syllables is a messy business because the English language is messy. For instance, why does “walked” have
+#one syllable while “loaded” is two? English is full of such inconsistencies.
+#For problems like this we don’t write algorithms, we write heuristics. A heuristic is a lot like an algorithm, only it’s
 not a 100% solution. It might, for instance, solve a problem with a good answer, but not necessarily a perfect
 answer.
+def count_syllables(words):
+    count = 0
+for word in words:
+word_count = count_syllables_in_word(word)
+count = count + word_count
+return count
+def count_syllables_in_word(word):
+count = 0
+return count
