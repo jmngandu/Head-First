e8f7982cb31fbab74fa6c242d84ef1be7543ce08
James Ngandu <jamesmngandu@gmail.com>
2023-06-13 23:39:56 +0300

    module

--- python/ch7/main.py
+++ python/ch7/main.py
@@ -1,2 +1,2 @@
 if __name__ == '__main__':#Test to see if this is the main program, and if so, supply whatever code you want here.
-    print("Look, I'm the main program y'all.")
+    print("Look, I'm the main program y'all.") #The important thing to note is this print statement will be ignored if this file is being imported.
