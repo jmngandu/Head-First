a675b77699689752344c103bfaf4f31bd2b2ea78
James Ngandu <jamesmngandu@gmail.com>
2023-06-17 18:13:25 +0300

    sum

--- python/ch8/sum.py
+++ python/ch8/sum.py
@@ -1,2 +1,8 @@
 marbles = [98, 78, 76, 87, 96, 45, 52]
 print('The total is ', sum(marbles))
+def compute_sum(list):
+    sum = 0
+    for number in list:
+        sum = sum + number
+    return sum
+print('The total is', compute_sum(marbles))
