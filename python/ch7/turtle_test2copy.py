346887ca185806ca5e53b8b9d3433469d888bdc1
James Ngandu <jamesmngandu@gmail.com>
2023-06-17 16:50:42 +0300

    circle

--- python/ch7/turtle_test2copy.py
+++ python/ch7/turtle_test2copy.py
@@ -2,8 +2,8 @@ import turtle
 slowpoke = turtle.Turtle()
 slowpoke.shape('turtle')
 def make_square(the_turtle):
-    for i in range(0,4):
+    for i in range(1000):
         the_turtle.forward(100)
-        the_turtle.right(90)
+        the_turtle.right(144)
 make_square(slowpoke)
 turtle.mainloop()
---
+++ python/ch7/turtle_test4.py
@@ -0,0 +1,15 @@
+import turtle
+slowpoke = turtle.Turtle()
+
+slowpoke.pencolor('blue')
+slowpoke.penup()
+slowpoke.setposition(-120, 0)
+slowpoke.pendown()
+slowpoke.circle(50)
+slowpoke.pencolor('red')
+slowpoke.penup()
+slowpoke.setposition(120, 0)
+slowpoke.pendown()
+slowpoke.circle(50)
+
+turtle.mainloop()
