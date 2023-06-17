0720bfab78955ff53bfc1971664348314fb6dea0
James Ngandu <jamesmngandu@gmail.com>
2023-06-17 15:03:34 +0300

    tutle

---
+++ python/ch7/race.py
@@ -0,0 +1,6 @@
+import turtle
+import random
+turtle = list()
+def setup(): #Let's define a function called setup to create and position our turtles
+    global turtles
+    startline = -480 #The startline variable just holds the x coordinate of the starting line; you'll see how this is used below
