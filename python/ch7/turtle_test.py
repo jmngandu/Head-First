7c654cdaafa9367d2e251f969fa037e53d58c566
James Ngandu <jamesmngandu@gmail.com>
2023-06-17 10:05:24 +0300

    turtle

---
+++ python/ch7/turtle_test.py
@@ -0,0 +1,12 @@
+import turtle
+slowpoke = turtle.Turtle()
+slowpoke.shape('turtle')
+slowpoke.forward(100)
+slowpoke.right(90)
+slowpoke.forward(100)
+slowpoke.right(90)
+slowpoke.forward(100)
+slowpoke.right(90)
+slowpoke.forward(100)
+slowpoke.right(90)
+turtle.mainloop()
