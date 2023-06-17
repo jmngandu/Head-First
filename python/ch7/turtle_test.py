c2e40d18dcd5cde0d338d5def3121de7513d446c
James Ngandu <jamesmngandu@gmail.com>
2023-06-17 10:34:43 +0300

    turtle

--- python/ch7/turtle_test.py
+++ python/ch7/turtle_test.py
@@ -1,12 +1,15 @@
 import turtle
+
 slowpoke = turtle.Turtle()
 slowpoke.shape('turtle')
-slowpoke.forward(100)
-slowpoke.right(90)
-slowpoke.forward(100)
-slowpoke.right(90)
-slowpoke.forward(100)
-slowpoke.right(90)
-slowpoke.forward(100)
-slowpoke.right(90)
+pokey = turtle.Turtle()
+pokey.shape('turtle')
+pokey.color('red')
+def make_square(the_turtle):
+  for i in range(0,4):
+    the_turtle.forward(100)
+    the_turtle.right(90)
+make_square(slowpoke)
+pokey.right(45)
+make_square(pokey)
 turtle.mainloop()
