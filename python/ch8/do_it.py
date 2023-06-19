3b91694d79e2f8a951be45a3aea7db823e515243
James Ngandu <jamesmngandu@gmail.com>
2023-06-19 23:40:56 +0300

    do_it

--- python/ch8/do_it.py
+++ python/ch8/do_it.py
@@ -1,4 +1,11 @@
 users = {}
+
+attributes = {
+'email' : 'kim@oreilly.com',
+'gender': 'f',
+'age': 27,
+'friends': ['John', 'Josh']}
+users['Kim'] = attributes
 users['John'] = {'email' : 'john@abc.com','gender': 'm', 'age': 24, 'friends': ['Kim', 'Josh']}
 users['Josh'] = {'email' : 'josh@wickedlysmart.com','gender': 'm', 'age': 32, 'friends': ['Kim']}
 print(users)
