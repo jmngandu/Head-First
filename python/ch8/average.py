e369310164e0598ba63382ecc3b77f2b8ca2ef79
James Ngandu <jamesmngandu@gmail.com>
2023-06-19 23:48:45 +0300

    average

---
+++ python/ch8/average.py
@@ -0,0 +1,11 @@
+users = {}
+users['Kim'] = {'email' : 'kim@oreilly.com','gender': 'f', 'age': 27, 'friends': ['John', 'Josh']}
+users['John'] = {'email' : 'john@abc.com','gender': 'm', 'age': 24, 'friends': ['Kim', 'Josh']}
+users['Josh'] = {'email' : 'josh@wickedlysmart.com','gender': 'm', 'age': 32, 'friends': ['Kim']}
+
+
+
+
+average_age('Kim')
+average_age('John')
+average_age('Josh')
