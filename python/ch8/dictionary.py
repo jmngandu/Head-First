cfc1de2c8e9f829329c75be8ee7035034cae4857
James Ngandu <jamesmngandu@gmail.com>
2023-06-19 23:26:25 +0300

    email

---
+++ python/ch8/dictionary.py
@@ -0,0 +1,9 @@
+names = ['Kim', 'John', 'Josh']
+emails = ['kim@oreilly.com', 'john@abc.com', 'josh@wickedlysmart.com']
+users = {'Kim' : 'kim@oreilly.com',
+    'John': 'john@abc.com',
+    'Josh': 'josh@wickedlysmart.com'}
+users['Avary'] = 'avary@gmail.com'
+del users['John']
+if 'Josh' in users:
+    print("Josh's email address is:", users['Josh'])
