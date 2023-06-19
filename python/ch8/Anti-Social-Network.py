179ea06462bee913968580aaa6526b1427843d75
James Ngandu <jamesmngandu@gmail.com>
2023-06-19 22:59:45 +0300

    network

---
+++ python/ch8/Anti-Social-Network.py
@@ -0,0 +1,9 @@
+my_dictionary = {}
+my_dictionary['jenny'] = '126-4344'
+my_dictionary['james'] = '600-7004'
+my_dictionary['paul'] = '555-1201'
+my_dictionary['david'] = '321-6617'
+my_dictionary['jamie'] = '771-0091'
+
+phone_number = my_dictionary['jenny']
+print('jennis number is ',phone_number)
