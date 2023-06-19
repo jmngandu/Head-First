37d37856fa3fee67d922d8ac3c19396da6b694da
James Ngandu <jamesmngandu@gmail.com>
2023-06-19 21:20:03 +0300

    added

--- python/ch8/mnyama.py
+++ python/ch8/mnyama.py
@@ -1,8 +1,10 @@
 marbles = [10, 23, 24, 65, 45]
 print("the total sum is = ", sum(marbles))
-def find_sum(list):
+
+def mukuvi_sum(list):
     sum = 0
     for number in list:
         sum = sum + number
-    return sum
-print(find_sum(marbles))
+        return sum
+print(mukuvi_sum(marbles))
+
