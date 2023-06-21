323909f6dbbbc54f9609b8a8de7da811702bfa04
James Ngandu <jamesmngandu@gmail.com>
2023-06-21 22:51:39 +0300

    loads

--- python/ch10/json_test.py
+++ python/ch10/json_test.py
@@ -1,5 +1,6 @@
 import json
 #And here’s a string containing JSON Note, to Python, this is just a string with some text it.
-json_string = '{"first": "james", "last":, "Ngandu", "prefix":, "Dr."}'
+json_string = '{"first": "james", "last": "Ngandu", "prefix": "Dr."}'
 #So let’s call json.loads on the string, and assign the resulting dictionary to the variable name.
 name = json.loads(json_string)
+print(name['prefix'], name['first'], name['last'])
