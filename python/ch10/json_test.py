07103290341be6e65286a343c25e960878b85e43
James Ngandu <jamesmngandu@gmail.com>
2023-06-21 22:53:12 +0300

    loads

--- python/ch10/json_test.py
+++ python/ch10/json_test.py
@@ -3,4 +3,5 @@ import json
 json_string = '{"first": "james", "last": "Ngandu", "prefix": "Dr."}'
 #So let’s call json.loads on the string, and assign the resulting dictionary to the variable name.
 name = json.loads(json_string)
+#And finally let’s use the Python dictionary to access the first and last name attributes along with the prefix attribute.
 print(name['prefix'], name['first'], name['last'])
