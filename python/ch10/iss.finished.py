719f4ee7bbd15d1f24f9765dac3d2433203a526a
James Ngandu <jamesmngandu@gmail.com>
2023-06-21 23:44:23 +0300

    Finishing off the ISS code

---
+++ python/ch10/iss.finished.py
@@ -0,0 +1,24 @@
+import requests, json, turtle
+def move_iss(lat, long):
+    global iss
+    iss.penup()
+    iss.goto(long, lat)
+    iss.pendown()
+screen = turtle.Screen()
+screen.setup(1000,500)
+screen.bgpic('earth.gif')
+screen.setworldcoordinates(-180, -90, 180, 90)
+iss = turtle.Turtle()
+turtle.register_shape("iss.gif")
+iss.shape("iss.gif")
+url = 'http://api.open-notify.org/iss-now.json'
+response = requests.get(url)
+if (response.status_code == 200):
+    response_dictionary = json.loads(response.text)
+    position = response_dictionary['iss_position']
+    lat = float(position['latitude'])
+    long = float(position['longitude'])
+    move_iss(lat, long)
+else:
+    print("Houston, we have a problem:", response.status_code)
+turtle.mainloop()
