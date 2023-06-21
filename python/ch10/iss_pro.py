79d8ba403f1eff76a3cf149f33fc458273841567
James Ngandu <jamesmngandu@gmail.com>
2023-06-21 23:28:06 +0300

    earth

--- python/ch10/iss_pro.py
+++ python/ch10/iss_pro.py
@@ -1,4 +1,18 @@
 import requests,  json,turtle
 #This first line just gets a reference to the turtle’s screen object.
 screen = turtle.screen()
-
+#We’re going to increase the size of the window to 1000 by 500 pixels, which matches the size of the image we’re about to add.
+screen.setup(1000,500)
+#This sets the entire window’s background to an image.
+screen.bgpic('earth.gif')
+#This resets the coordinate system of the turtle window; we talk about this on the next page
+url = 'http://api.open-notify.org/iss-now.json'
+response = requests.get(url)
+if (response.status_code == 200):
+    response_dictionary = json.loads(response.text)
+    position = response_dictionary['iss_position']
+    print('International Space Station at ' +
+        position['latitude'] + ', ' + position['longitude'])
+else:
+    print("Houston, we have a problem:", response.status_code)
+turtle.mainloop()
